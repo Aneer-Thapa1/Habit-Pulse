6d0b339b52056faed86dc25097eaebdab619ab80
Anir Jung Thapa <anir234thapa@gmail.com>
2024-03-31 17:39:04 +0545

    setup

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div class="bg-red-700 font-black flex justify-between h-10 items-center">
+      <h5>This is navbar</h5>
+      <div>
+        <img class="w-9 h-9 rounded-full" src="https://avatars.githubusercontent.com/u/134839446?v=4" alt="profile picture" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './../components/Navbar';
+
+
+
+const Home = () => {
+  return (
+    <div >
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
