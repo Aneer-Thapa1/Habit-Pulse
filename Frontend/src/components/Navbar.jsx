6673dd74810fa0681d6939add6912eb9ec1bf131
Anir Jung Thapa <anir234thapa@gmail.com>
2024-04-01 20:27:02 +0545

    started

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div class="bg-red-700 font-black flex justify-between h-10 items-center">
+    <div class="bg-white-700 font-black flex justify-between h-10 w-40 items-center">
       <h5>This is navbar</h5>
       <div>
         <img class="w-9 h-9 rounded-full" src="https://avatars.githubusercontent.com/u/134839446?v=4" alt="profile picture" />
